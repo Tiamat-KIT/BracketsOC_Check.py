784cd41f7a93816401531a24092000d0bf80c1b4
Amo_Taiki_from_Desktop <c1101081@planet.kanazawa-it.ac.jp>
2023-10-30 12:05:34 +0900

    おめえ結局これどうすればええんだ！！

--- main.py
+++ main.py
@@ -1,67 +1,50 @@
 import re
 import sys
 
-def main(st):
-    InputStringList = list(st)
-    """
-    Count配列の4つの要素を左から解説
-    ・波かっこの開き閉じの個数
-    ・丸かっこの開き閉じの個数
-    ・四角かっこの開き閉じの個数
-    ・改行文字の個数
-    """
-    Count = [0,0,0,0]
-    StringLengthCount = 0
-    line_num = 1
+# if __name__ == "__main__":
+if len(sys.argv)==2:
+    with open(sys.argv[1], 'r') as InFile:
+        with open("./output.txt","w") as out:
+            lines = InFile.readlines()
+            output_row_num = 1
+            open_wave = 0
+            open_square = 0
+            open_circle = 0
+            count = 0
 
-    for i in InputStringList:
-        if(re.match(r"\{",i)):
-            # print("波かっこの展開が認められました")
-            Count[0] += 1
+            for i in range(len(lines)):
+                st = lines[i]
+                output_row_num += 1
 
-        if(re.match(r"\}",i)):
-            # print("ちゃんと閉じててえらい！")
-            Count[0] += 1
+                for j in st:
+                    if(re.match(r"\{",j)):
+                        open_wave = 1
+                        count += 1
+                    elif(re.match(r"\)|\]",j) and open_wave == 1):
+                        raise ValueError("波括弧が正常に閉じられていない可能性があります")
+                    elif(re.match(r"\}",j)):
+                        open_wave = 0
+                        count -= 1
 
-        if(re.match(r"\(",i)):
-            # print("かっこの展開が認められました")
-            Count[1] += 1
-
-        if(re.match(r"\)",i)):
-            # print("ちゃんと閉じててえらい！")
-            Count[1] += 1
-        
-        if(re.match(r"\[",i)):
-            # print("角かっこの展開が認められました")
-            Count[2] += 1
-
-        if(re.match(r"\]",i)):
-            # print("ちゃんと閉じててえらい！")
-            Count[2] += 1
-        
-        if(re.match(r"\n",i)):
-            # print("改行コードが検出されました。")
-            Count[3] += 1
-
-        StringLengthCount += 1
-        # if(StringLengthCount == len(InputText)):
-        if(StringLengthCount == len(st)):
-            if Count[0] % 2 != 0: raise ValueError("波括弧が正常に閉じられていない可能性があります")
-            elif Count[1] % 2 != 0: raise ValueError("かっこが正常に閉じられていない可能性があります") 
-            elif Count[2] % 2 != 0: raise ValueError("角かっこが正常に閉じられていない可能性があります")
-            else: 
-                print("---------------------------------------------\n 与えられた文字の検証を終了します。改行の数は"+ str(Count[3]) +"回でした\n---------------------------------------------\n")
-
-if __name__ == "__main__":
-    if len(sys.argv)==2:
-        with open(sys.argv[1], 'r') as f:
-            lines = f.readlines()
-            st=[line for line in lines]
-            print(st)
-            line_num = 1
-            for i in st:
-                main(i)
-                with open("./output.txt","w") as out:
-                    out.white(str(line_num) + ":" + st)
-                    line_num += 1
+                    if(re.match(r"\(",j)):
+                        open_circle = 1
+                        count += 1
+                    elif(re.match(r"\]|\}",j) and open_circle == 1):
+                        raise ValueError("かっこが正常に閉じられていない可能性があります")
+                    elif(re.match(r"\)",j)):
+                        open_circle = 0
+                        count -= 1
+                
+                    if(re.match(r"\[",j)):
+                        open_square = 1
+                        count += 1
+                    elif(re.match(r"\}|\)",j) and open_square == 1):
+                        raise ValueError("角かっこが正常に閉じられていない可能性があります")
+                    elif(re.match(r"\]",j)):
+                        open_square = 0
+                        count -= 1
                     
+                    if count < 0:
+                        raise ValueError("括弧が最初に閉じられている可能性があります")
+                    
+                out.writelines(str(output_row_num) + "(" + str(count) + ") :" + st)
