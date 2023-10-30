5c15376ec4c22df625451eb7f7ddcd9d07e6cfc4
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-10-30 23:40:13 +0000

    空白行削除

--- main.py
+++ main.py
@@ -87,4 +87,3 @@ if len(sys.argv)==2:
                             open["square"] -= 1
                             op["block"] -= 1
                         store_stack.append(char)
-                        
